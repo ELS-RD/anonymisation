b068ba44aff6137d3554eb4155c1a2f2eac5c985
pommedeterresautee <pommedeterresautee@msn.com>
2019-09-04 22:57:59 +0200

    better data generation

--- flair_generate_data.py
+++ flair_generate_data.py
@@ -31,42 +31,46 @@ random.seed(123)
 tagger: SequenceTagger = SequenceTagger.load('resources/flair_ner/ca/best-model.pt')
 
 config_training = get_config_default()
-xml_train_path = config_training["xml_train_path"]
-
 nlp = spacy.blank('fr')
 nlp.tokenizer = get_tokenizer(nlp)
 tokenizer = build_spacy_tokenizer(nlp)
-files = os.listdir(xml_train_path)
 
+xml_train_path = "../similar_legal_case/data/jurica_original_xml/arrets-juri"  # config_training["xml_train_path"]
+files = [os.path.join(path, name) for path, _, files in os.walk(xml_train_path) for name in files]
+random.shuffle(files)
 
 with open("./resources/training_data/generated_annotations.txt", mode='w') as generated_text:
     with open("./resources/training_data/generated_annotations.ent", mode='w') as generated_entities:
         for filename in files:
             if filename.endswith(".xml"):
-                print(f"--- {filename} ---")
-                text_lines: List[str] = list()
-                offset_lines: List[str] = list()
-                current_path = os.path.join(xml_train_path, filename)
-                generated_paragraphs: List[Sentence] = list()
-                paragraphs = get_paragraph_from_file(path=current_path,
-                                                     keep_paragraph_without_annotation=True)
-                if len(paragraphs) > 50000:
-                    for paragraph in paragraphs:  # type: Paragraph
-                        if len(paragraph.text) > 0:
-                            s = Sentence(text=paragraph.text, tokenizer=tokenizer)
-                            generated_paragraphs.append(s)
+                try:
+                    print(f"--- {filename} ---")
+                    text_lines: List[str] = list()
+                    offset_lines: List[str] = list()
+                    print("read XML")
+                    generated_paragraphs: List[Sentence] = list()
+                    paragraphs = get_paragraph_from_file(path=filename,
+                                                         keep_paragraph_without_annotation=True)
+                    if len(paragraphs) > 50000:
+                        for paragraph in paragraphs:  # type: Paragraph
+                            if len(paragraph.text) > 0:
+                                s = Sentence(text=paragraph.text, tokenizer=tokenizer)
+                                generated_paragraphs.append(s)
 
-                    generated_paragraphs = tagger.predict(sentences=generated_paragraphs,
-                                                          mini_batch_size=32,
-                                                          verbose=True,
-                                                          embedding_storage_mode="none")
+                        generated_paragraphs = tagger.predict(sentences=generated_paragraphs,
+                                                              mini_batch_size=32,
+                                                              verbose=True,
+                                                              embedding_storage_mode="none")
 
-                    for sentence in generated_paragraphs:
-                        text_lines.append(sentence.original_text)
-                        offset_line = [f"{tag.start_pos} {tag.end_pos} {tag.tag}" for tag in sentence.get_spans('ner')]
-                        offset_lines.append(",".join(offset_line))
-                    assert len(offset_lines) == len(text_lines)
-                    assert len(offset_lines) > 0
-                    print("exporting...")
-                    generated_text.write("\n".join(text_lines))
-                    generated_entities.write("\n".join(offset_lines))
+                        for sentence in generated_paragraphs:
+                            text_lines.append(sentence.original_text)
+                            offset_line = [f"{tag.start_pos} {tag.end_pos} {tag.tag}" for tag in
+                                           sentence.get_spans('ner')]
+                            offset_lines.append(",".join(offset_line))
+                        assert len(offset_lines) == len(text_lines)
+                        assert len(offset_lines) > 0
+                        print("exporting...")
+                        generated_text.write("\n".join(text_lines))
+                        generated_entities.write("\n".join(offset_lines))
+                except Exception as e:
+                    print(f"caught an exception: {e}")
